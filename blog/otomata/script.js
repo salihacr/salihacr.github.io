ca56482c9cd2640db693a618020df698a842dc19
Salih ACUR <salihacur1@gmail.com>
2020-12-31 14:39:54 +0300

    automat edited

--- blog/otomata/script.js
+++ blog/otomata/script.js
@@ -1,12 +1,12 @@
 // State Object
 const stateObject = {
     states: [
-        { stateId: "q0 (Durum 0)", northLight: 3, westLight: 1, delay: 0 },
-        { stateId: "q1 (Durum 1)", northLight: 2, westLight: 1, delay: 5000 },
-        { stateId: "q2 (Durum 2)", northLight: 1, westLight: 2, delay: 7000 },
-        { stateId: "q3 (Durum 3)", northLight: 1, westLight: 3, delay: 9000 },
-        { stateId: "q4 (Durum 4)", northLight: 1, westLight: 2, delay: 14000 },
-        { stateId: "q5 (Durum 5)", northLight: 2, westLight: 1, delay: 16000 }
+        { stateId: 'q0 (Durum 0)', northLight: 3, westLight: 1, delay: 0 },
+        { stateId: 'q1 (Durum 1)', northLight: 2, westLight: 1, delay: 5000 },
+        { stateId: 'q2 (Durum 2)', northLight: 1, westLight: 2, delay: 7000 },
+        { stateId: 'q3 (Durum 3)', northLight: 1, westLight: 3, delay: 9000 },
+        { stateId: 'q4 (Durum 4)', northLight: 1, westLight: 2, delay: 14000 },
+        { stateId: 'q5 (Durum 5)', northLight: 2, westLight: 1, delay: 16000 }
     ]
 };
 
@@ -15,47 +15,89 @@ const lamps = document.querySelectorAll('.lamp');
 const lampsEast = document.querySelectorAll('.lamp2');
 const stateText = document.getElementById('state');
 const button = document.getElementById('simulate');
+const alertElement = document.getElementById('alert');
 
-const colors2 = ['red', 'yellow', 'green'];
+const colorNames = ['red', 'yellow', 'green'];
 const colors = ['#c0392b', '#f1c40f', '#2ecc71'];
+
+const len = stateObject.states.length;
+console.log(len);
+
+// Set off times
+const offTimes = [];
+stateObject.states.forEach(state => {
+    offTimes.push(state.delay);
+});
+offTimes[offTimes.length] = offTimes[offTimes.length - 1] + 2001;
+console.log(offTimes);
+
+const stateArr = stateObject.states;
+console.log(stateArr);
+
+// Helper functions
+function createAlert(alertElement, text, ms) {
+    alertElement.innerText = text;
+    alertElement.style.display = 'block';
+    setTimeout(() => {
+        alertElement.style.display = 'none';
+    }, ms);
+}
+
+let onLights = function (states, index) {
+    lamps[states[index].northLight - 1].style.backgroundColor = colors[states[index].northLight - 1];
+    lamps[states[index].northLight - 1].style.boxShadow = `0 0 10px 5px ${colors[states[index].northLight - 1]}`;
+
+    console.log(`On : ${states[index].stateId}`);
+    stateText.innerText = 'Şuanki Durum : ' + states[index].stateId;
+
+    lampsEast[states[index].westLight - 1].style.backgroundColor = colors[states[index].westLight - 1];
+    lampsEast[states[index].westLight - 1].style.boxShadow = `0 0 10px 5px ${colors[states[index].westLight - 1]}`;
+}
+
+let offLights = function (states, index) {
+    lamps[states[index].northLight - 1].style.backgroundColor = '#121212';
+    lamps[states[index].northLight - 1].style.boxShadow = ``;
+
+    console.log(`Off : ${states[index].stateId}`);
+
+    lampsEast[states[index].westLight - 1].style.backgroundColor = '#121212';
+    lampsEast[states[index].westLight - 1].style.boxShadow = ``;
+}
+
+onLights(stateArr, 0);
 
 // Simulation Button Click Event
 button.addEventListener('click', () => {
     startSimulate();
-    setInterval(() => {
+    console.log('Simulasyon başlatıldı.');
+
+    // If you want to run it only once.
+    setTimeout(() => {
+        onLights(stateArr, 0);
+        console.log('Simülasyon sonu, tüm durumlar başarı ile tamamlandı.');
+        createAlert(alertElement, 'Simülasyon başarı ile tamamlandı!', 2000);
+        button.disabled = false;
+        button.style.cursor = 'pointer';
+    }, offTimes[offTimes.length - 1]);
+
+    // If you want to run it continuously.
+    /*setInterval(() => {
         startSimulate();
-    }, 18001);
+    }, offTimes[offTimes.length - 1]);*/ //18001);
 });
 
-const offTimes = [0, 5000, 7000, 9000, 14000, 16000, 18001];
-
 // startSimulate simulate
-let startSimulate = function() {
-    const len = stateObject.states.length;
-    console.log(len);
+let startSimulate = function () {
+    createAlert(alertElement, 'Simulasyon başlatıldı.', 2000);
+    button.disabled = true;
+    button.style.cursor = 'wait';
     for (let i = 0; i < len; i++) {
         setTimeout(function () {
-
-            lamps[stateObject.states[i].northLight - 1].style.backgroundColor = colors[stateObject.states[i].northLight - 1];
-            lamps[stateObject.states[i].northLight - 1].style.boxShadow = `0 0 10px 5px ${colors[stateObject.states[i].northLight - 1]}`;
-
-            console.log("on", stateObject.states[i]);
-            stateText.innerText = "Şuanki Durum : " + stateObject.states[i].stateId;
-            lampsEast[stateObject.states[i].westLight - 1].style.backgroundColor = colors[stateObject.states[i].westLight - 1];
-            lampsEast[stateObject.states[i].westLight - 1].style.boxShadow = `0 0 10px 5px ${colors[stateObject.states[i].westLight - 1]}`;
-
+            onLights(stateArr, i);
         }, offTimes[i]);//stateObject.states[i].delay);
 
         setTimeout(() => {
-
-            lamps[stateObject.states[i].northLight - 1].style.backgroundColor = '#121212';
-            lamps[stateObject.states[i].northLight - 1].style.boxShadow = ``;
-
-            console.log("off", stateObject.states[i]);
-
-            lampsEast[stateObject.states[i].westLight - 1].style.backgroundColor = '#121212';
-            lampsEast[stateObject.states[i].westLight - 1].style.boxShadow = ``;
-
+            offLights(stateArr, i);
         }, offTimes[i + 1]);//stateObject.states[i + 1].delay);
     }
 }
